918e01b40c64379acde981b65ee36d9c2e56663b
Bruno De Deken <dedekenbruno@gmail.com>
2022-04-22 09:21:59 +0200

    Optimize scripts

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
-gcloud run deploy dash-demo \
---image=europe-west1-docker.pkg.dev/my-demo-project-yuhijk/dash-repo/dash-demo:latest \
---platform=managed \
---project=my-demo-project-yuhijk \
---region=europe-west1 \
---timeout=60 \
---concurrency=10 \
---cpu=1 \
---max-instances=4 \
---allow-unauthenticated
+PROJECT_ID=${1}
+if [ -z "$PROJECT_ID" ]
+  then
+    echo "No project ID supplied."
+  else
+    echo "Deploying dash demo to project ${PROJECT_ID}"
+
+    gcloud run deploy dash-demo \
+    --image=europe-west1-docker.pkg.dev/${PROJECT_ID}/dash-repo/dash-demo:latest \
+    --platform=managed \
+    --project=${PROJECT_ID} \
+    --region=europe-west1 \
+    --timeout=60 \
+    --concurrency=10 \
+    --cpu=1 \
+    --max-instances=4 \
+    --allow-unauthenticated
+fi
+
 
 
 
--- push_docker.sh
+++ push_docker.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+PROJECT_ID=${1}
+if [ -z "$PROJECT_ID" ]
+  then
+    echo "No project ID supplied."
+  else
+    echo "Build, tag and push docker image"
+    docker build --platform=linux/amd64 --tag europe-west1-docker.pkg.dev/my-demo-project-n6s1/dash-repo/dash-demo:latest -t dash-demo .
 
-echo "Build, tag and push docker image"
-docker build --platform=linux/amd64 --tag europe-west1-docker.pkg.dev/my-demo-project-yuhijk/dash-repo/dash-demo:latest -t dash-demo .
-
-echo "Push image to GCP container registry"
-docker push europe-west1-docker.pkg.dev/my-demo-project-yuhijk/dash-repo/dash-demo:latest
-
+    echo "Push image to project ${PROJECT_ID}'s artifact registry"
+    docker push europe-west1-docker.pkg.dev/my-demo-project-n6s1/dash-repo/dash-demo:latest
+fi
