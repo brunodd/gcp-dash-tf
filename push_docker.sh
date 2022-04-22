bc1925a100298cba0f79cd431e7dd7d241f25831
Bruno De Deken <dedekenbruno@gmail.com>
2022-04-22 14:03:41 +0200

    Bugfix

--- push_docker.sh
+++ push_docker.sh
@@ -4,8 +4,8 @@ if [ -z "$PROJECT_ID" ]
     echo "No project ID supplied."
   else
     echo "Build, tag and push docker image"
-    docker build --platform=linux/amd64 --tag europe-west1-docker.pkg.dev/my-demo-project-n6s1/dash-repo/dash-demo:latest -t dash-demo .
+    docker build --platform=linux/amd64 --tag europe-west1-docker.pkg.dev/${PROJECT_ID}/dash-repo/dash-demo:latest -t dash-demo .
 
     echo "Push image to project ${PROJECT_ID}'s artifact registry"
-    docker push europe-west1-docker.pkg.dev/my-demo-project-n6s1/dash-repo/dash-demo:latest
+    docker push europe-west1-docker.pkg.dev/${PROJECT_ID}/dash-repo/dash-demo:latest
 fi
